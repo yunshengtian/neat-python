21ceb0b4ac2a511cfd86e5d3da180f0fc70d65a3
Yunsheng Tian <yunsheng@mit.edu>
2021-05-02 23:36:36 -0400

    make parallel evaluator also evaluates constraint

--- neat/parallel.py
+++ neat/parallel.py
@@ -6,14 +6,18 @@ from multiprocessing import Pool
 
 
 class ParallelEvaluator(object):
-    def __init__(self, num_workers, eval_function, timeout=None):
+    def __init__(self, num_workers, fitness_function, constraint_function=None, timeout=None):
         """
-        eval_function should take one argument, a tuple of
+        fitness_function should take one argument, a tuple of
         (genome object, config object), and return
         a single float (the genome's fitness).
+        constraint_function should take one argument, a tuple of
+        (genome object, config object), and return
+        a single bool (the genome's validity).
         """
         self.num_workers = num_workers
-        self.eval_function = eval_function
+        self.fitness_function = fitness_function
+        self.constraint_function = constraint_function
         self.timeout = timeout
         self.pool = Pool(num_workers)
 
@@ -21,11 +25,22 @@ class ParallelEvaluator(object):
         self.pool.close() # should this be terminate?
         self.pool.join()
 
-    def evaluate(self, genomes, config):
+    def evaluate_fitness(self, genomes, config):
         jobs = []
         for ignored_genome_id, genome in genomes:
-            jobs.append(self.pool.apply_async(self.eval_function, (genome, config)))
+            jobs.append(self.pool.apply_async(self.fitness_function, (genome, config)))
 
         # assign the fitness back to each genome
         for job, (ignored_genome_id, genome) in zip(jobs, genomes):
             genome.fitness = job.get(timeout=self.timeout)
+
+    def evaluate_constraint(self, genomes, config):
+        jobs = []
+        for ignored_genome_id, genome in genomes:
+            jobs.append(self.pool.apply_async(self.constraint_function, (genome, config)))
+
+        # return the validity of each genome
+        validity = []
+        for job, (ignored_genome_id, genome) in zip(jobs, genomes):
+            validity.append(job.get(timeout=self.timeout))
+        return validity
