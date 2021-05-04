6072433dbbe3b37b226c390c7cd7ac4930fe6db3
Yunsheng Tian <yunsheng@mit.edu>
2021-05-04 01:48:25 -0400

    add BaseReporter to __init__

--- neat/__init__.py
+++ neat/__init__.py
@@ -9,7 +9,7 @@ from neat.population import Population, CompleteExtinctionException
 from neat.genome import DefaultGenome
 from neat.reproduction import DefaultReproduction
 from neat.stagnation import DefaultStagnation
-from neat.reporting import StdOutReporter
+from neat.reporting import BaseReporter, StdOutReporter
 from neat.species import DefaultSpeciesSet
 from neat.statistics import StatisticsReporter
 from neat.parallel import ParallelEvaluator
